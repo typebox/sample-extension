ce379bcf3475c6794fd7ab58a6a00bc801af96b7
Aaron Job <aaron@gravypower.net>
2020-10-24 18:39:04 +1100

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-module.exports = function (cb) {
-  cb(null, 'hello world!');
-}
+var express  = require('express');
+var app      = express();
+
+app.use((req, res, next) => {
+  console.log(req;
+  next();
+});
+
+
+module.exports = app;
